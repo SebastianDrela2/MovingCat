aa27d7a207f0ff9529fcd1813e82585f846577c4
Sebastian Drela <sebastian.drela@wp.pl>
2023-05-21 16:37:34 +0200

    classes for box finding game

---
+++ Box.cs
@@ -0,0 +1,81 @@
+﻿namespace retardcat
+{
+    public class Box
+    {
+        public int BoxNumber;
+        public bool HasCat;
+        public Box? Next;
+        public Box Before;
+
+        public Box(ref int start, ref int ammount)
+        {
+            if (start.Equals(ammount))
+            {
+                Next = null;
+            }
+            else
+            {
+                BoxNumber = start;
+                start++;
+                Next = new Box(ref start, ref ammount);
+                Next.Before = this;
+            }
+        }
+
+        public static Box? AsignCatToOneOfBoxes(int maxNumber, Box box)
+        {
+            var random = new Random();
+            var radomNumber = random.Next( maxNumber+ 1);
+
+            while (box.Next != null)
+            {
+                if (box.BoxNumber.Equals(radomNumber))
+                {
+                    box.HasCat = true;
+                    return box;
+                }
+                box = box.Next;
+            }
+
+            return null;
+        }
+
+        public static Box? FindFirstCatForReference(Box box)
+        {
+            while (box.Next != null)
+            {
+                if (box.HasCat = true)
+                {
+                    return box;
+                }
+                box = box.Next;
+            }
+
+            return null;
+        }
+        public static bool LookForCat(Box box)
+        {
+            if (box.HasCat.Equals(true))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static Box? MoveCat(Box catBox)
+        {
+            var random = new Random();
+            var randomNumber = random.Next(2);
+            catBox.HasCat = false;
+            if (randomNumber.Equals(1) && catBox.Next != null)
+            {
+                catBox.Next.HasCat = true;
+                return catBox.Next;
+            }
+
+            catBox.Before.HasCat = true;
+            return catBox.Before;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿namespace retardcat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = 0;
+            var ammount = 10000;
+
+            var box = new Box(ref start,ref ammount);
+            var cat = Box.AsignCatToOneOfBoxes(ammount,box);
+            var firstCat = Box.FindFirstCatForReference(cat);
+            var (found,ammounst,catbox) = CatGame(ref box, ref cat);
+
+            if (found.Equals(true))
+            {
+                Console.WriteLine($"Cat was found in {ammounst} moves at {catbox}");
+                Console.WriteLine($"FirstCat originall was in {firstCat.BoxNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"Cat was not found.");
+            }
+        }
+
+        private static (bool,int,int) CatGame(ref Box box, ref Box? catBox)
+        {
+            int ammountOfMoves = 0;
+
+            while (box.Next != null)
+            {
+                ammountOfMoves++;
+                if (Box.LookForCat(box))
+                {
+                    return (true,ammountOfMoves,box.BoxNumber);
+                }
+                catBox = Box.MoveCat(catBox);
+                if (box.BoxNumber % 2 == 0)
+                {
+                    ammountOfMoves++;
+                    if (Box.LookForCat(box))
+                    {
+                        return (true, ammountOfMoves,box.BoxNumber);
+                    }
+                }
+                box = box.Next;
+            }
+
+            while (box.BoxNumber != 0)
+            {
+                box = box.Before;
+                if (Box.LookForCat(box))
+                {
+                    return (true,ammountOfMoves,box.BoxNumber);
+                }
+
+                catBox = Box.MoveCat(catBox);
+                ammountOfMoves++;
+            }
+            return (false,0,0);
+        }
+    }
+}
