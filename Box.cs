45eb62bbcefc1f8c25b6f9300ddff536079a2847
Sebastian Drela <sebastian.drela@wp.pl>
2024-02-22 04:31:03 +0100

    cat box game

---
+++ Box.cs
@@ -0,0 +1,80 @@
+﻿namespace MovingCat
+{
+    public class Box
+    {
+        public int BoxNumber;
+        public bool HasCat;
+        public Box? Next;
+        public Box Before;
+
+        public Box(ref int start, ref int ammount)
+        {
+            if (start.Equals(ammount))
+            {
+                Next = null;
+            }
+            else
+            {
+                BoxNumber = start;
+                start++;
+
+                Next = new Box(ref start, ref ammount)
+                {
+                    Before = this
+                };
+            }
+        }
+
+        public static Box? AsignCatToOneOfBoxes(int maxNumber, Box box)
+        {
+            var random = new Random();
+            var randomNumber = random.Next(maxNumber + 1);
+
+            while (box.Next != null)
+            {
+                if (box.BoxNumber.Equals(randomNumber))
+                {
+                    box.HasCat = true;
+                    return box;
+                }
+
+                box = box.Next;
+            }
+
+            return null;
+        }
+
+        public static Box? FindFirstCatForReference(Box box)
+        {
+            while (box.Next != null)
+            {
+                if (box.HasCat)
+                {
+                    return box;
+                }
+
+                box = box.Next;
+            }
+
+            return null;
+        }
+
+        public static bool LookForCat(Box box) => box.HasCat.Equals(true);
+
+        public static Box? MoveCat(Box catBox)
+        {
+            var random = new Random();
+            var randomNumber = random.Next(2);
+            catBox.HasCat = false;
+
+            if (randomNumber.Equals(1) && catBox.Next != null)
+            {
+                catBox.Next.HasCat = true;
+                return catBox.Next;
+            }
+
+            catBox.Before.HasCat = true;
+            return catBox.Before;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿namespace MovingCat;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var start = 0;
+        var amount = 10000;
+
+        var box = new Box(ref start, ref amount);
+        var cat = Box.AsignCatToOneOfBoxes(amount, box);
+        var firstCat = Box.FindFirstCatForReference(cat);
+        var (found, ammount, catBox) = CatGame(ref box, ref cat);
+
+        if (found.Equals(true))
+        {
+            Console.WriteLine($"Cat was found in {ammount} moves at {catBox}");
+            Console.WriteLine($"FirstCat original was in {firstCat.BoxNumber}");
+        }
+        else
+        {
+            Console.WriteLine($"Cat was not found.");
+        }
+    }
+
+    private static (bool, int, int) CatGame(ref Box box, ref Box? catBox)
+    {
+        var amountOfMoves = 0;
+
+        while (box.Next != null)
+        {
+            amountOfMoves++;
+            if (Box.LookForCat(box))
+            {
+                return (true, amountOfMoves, box.BoxNumber);
+            }
+
+            catBox = Box.MoveCat(catBox);
+            if (box.BoxNumber % 2 == 0)
+            {
+                amountOfMoves++;
+                if (Box.LookForCat(box))
+                {
+                    return (true, amountOfMoves, box.BoxNumber);
+                }
+            }
+
+            box = box.Next;
+        }
+
+        while (box.BoxNumber != 0)
+        {
+            box = box.Before;
+            if (Box.LookForCat(box))
+            {
+                return (true, amountOfMoves, box.BoxNumber);
+            }
+
+            catBox = Box.MoveCat(catBox);
+            amountOfMoves++;
+        }
+
+        return (false, 0, 0);
+    }
+}
